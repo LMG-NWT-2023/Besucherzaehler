6254415291f0e48de2631a90c19f27d948295c77
Valentin361 <122107407+Valentin361@users.noreply.github.com>
2023-01-17 15:31:55 +0100

    Update SensorStateMachine.js

--- SensorStateMachine.js
+++ SensorStateMachine.js
@@ -1,6 +1,11 @@
 
 // Die möglichen Zustände - damit wir beim Entwickeln eine ordenliche Code-Completion Unterstützung bekommen. 
 
+const m_Inputs = "a"
+
+var verlassen = 0;
+var besucher
+
 const States = {
     S0: "S0",
     E1: "E1",
@@ -12,55 +17,111 @@ const States = {
     ERROR: "ERROR",
 }
 exports.States = States
-class StateMachine {
+class StateMachine
+{
     state = States.S0
-    besucher = 0
 
     log() {
         console.log(`SensorStateMachine.state: ${this.state} - Besucher: ${this.besucher}`)
-    }
+   }
 
-    input(newValues) {
+    input(newValues)
+     {
+        if(newValues.Sensor1 === 0 && newValues.Sensor2 === 0)
+        {
+            m_Inputs = ("a")
+        }
+        else if(newValues.Sensor1 === 1 && newValues.Sensor2 === 0)
+        {
+            m_Inputs = ("b")
+        }
+        else if(newValues.Sensor1 === 1 && newValues.Sensor2 === 1)
+        {
+            m_Inputs = ("c")
+        }
+        else if(newValues.Sensor1 === 0 && newValues.Sensor === 1)
+        {
+            m_Inputs = ("d")
+        }
+
         switch (this.state) {
-            case States.S0:
-                // zwei Zeige - Eingane und Ausgang
-                if (newValues.Sensor1 === 0 && newValues.Sensor2 === 1) {
-                    this.state = States.A1
-                } else if (newValues.Sensor1 === 1 && newValues.Sensor2 === 0) {
-                    this.state = States.E1
-                } else {
-                    this.state = States.S0
-                }
-                break
-            case States.A2:
-                if (newValues.Sensor1 === 0 && newValues.Sensor2 === 1) {
-                    this.state = States.A1
-                } else {
-                    this.state = States.ERROR
-                }
-                break
-            case States.A3:
-                if (newValues.Sensor1 === 0 && newValues.Sensor2 === 1) {
-                    this.state = States.A1
-                } else {
-                    this.state = States.ERROR
-                }
-                break
             case States.A1:
-                if (newValues.Sensor1 === 0 && newValues.Sensor2 === 1) {
-                    this.state = States.A1
-                } else {
-                    this.state = States.ERROR
+
+                //Cases für Verlassensprozess
+                if(m_Inputs === ("c"))
+                {
+                    this.state = States.A2
                 }
+                else if(m_Inputs === ("a") || m_Inputs === ("d"))
+                {
+                    this.state = States.E1
+                } 
                 break
+
             case States.A2:
-                if (newValues.Sensor1 === 0 && newValues.Sensor2 === 0) {
-                    this.state = States.S0
-                    this.besucher++
-                } else {
+
+                if(m_Inputs === ("d")) 
+                {
+                    this.state = States.A3
+                } 
+                else if(m_Inputs === ("a") || m_Inputs === ("b")){
                     this.state = States.ERROR
                 }
                 break
+
+            case States.A3:
+
+                if (m_Inputs === ("a"))
+                {
+                    this.state = States.S0	
+                    verlassen --;
+                }
+                break
+
+            case States.S0:
+
+                if(m_Input === ("b"))
+                {
+                    this.state = States.A1
+                } 
+                else if(m_Input === ("d")) 
+                {
+                    this.state = States.E1
+                }
+                break
+
+            case States.E1:
+
+                if(m_Input === ("c"))
+                {
+                    this.state = States.E2
+                }
+                else if(m_Inputs === ("a") || m_Inputs === ("b"))
+                {
+                    this.state = State.S0
+                }
+                break
+
+            case States.E2:
+
+            if(m_Input === ("b"))
+            {
+                this.state = State.E3
+            }
+            else if(m_Inputs === ("a") || m_Inputs === ("d"))
+            {
+                this.state = State.S0
+            }
+            break
+
+            case States.E3: 
+
+            if(m_Input === ("a"))
+            {
+                this.state = State.S0
+                besucher ++
+            }
+            break
 
             default:
                 this.state = States.ERROR
